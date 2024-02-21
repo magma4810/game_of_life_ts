8e205bc18ab8eaab3784047edee4ce01d4e5eeaf
Pavel <magma4810@gmail.com>
2024-02-21 21:41:41 +0300

    initial preparing

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      "jest/globals": true,
+    },
+    extends: [
+      "airbnb-base",
+      "plugin:jest/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    ignorePatterns : ["webpack.config.js"],
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+    },
+    rules: {
+      "import/prefer-default-export": "off",
+      "jest/valid-title": "off",
+      "no-console": "off",
+      "no-alert": "off",
+      "no-restricted-globals": "off",
+      "no-plusplus": "off",
+      "import/no-unresolved": "off", // https://github.com/typescript-eslint/typescript-eslint/issues/1624
+      "import/extensions": ["warn", "never"], // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+    },
+  };
+  
